97e893db2f82eb70f8866cb2cd28f8049b571fda
Er Jun Li <er.li@mail.mcgill.ca>
2021-10-18 01:25:01 -0400

    A1: Done point robot

---
+++ A1/point.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python rrt_planner_point_robot.py --start_pos_x 10 --start_pos_y 270 --target_pos_x 900 --target_pos_y 30 # --rrt_sampling_policy gaussian
--- A1/rrt_planner_point_robot.py
+++ A1/rrt_planner_point_robot.py
@@ -13,13 +13,13 @@ drawInterval = 100 # 10 is good for normal real-time drawing
 
 prompt_before_next=1  # ask before re-running sonce solved
 SMALLSTEP = args.step_size # what our "local planner" can handle.
-map_size,obstacles = imageToRects.imageToRects(args.world)
+map_size, obstacles = imageToRects.imageToRects(args.world)
 #Note the obstacles are the two corner points of a rectangle
 #Each obstacle is (x1,y1), (x2,y2), making for 4 points
 XMAX = map_size[0]
 YMAX = map_size[1]
 
-vertices = [ [args.start_pos_x, args.start_pos_y], [args.start_pos_x, args.start_pos_y + 10]]
+vertices = [[args.start_pos_x, args.start_pos_y], [args.start_pos_x, args.start_pos_y + 10]]
 
 # goal/target
 tx = args.target_pos_x
@@ -88,7 +88,7 @@ def returnParent(k, canvas, G):
             return e[0]
 
 def genvertex():
-    vertices.append( genPoint() )
+    vertices.append(genPoint())
     return len(vertices)-1
 
 def pointToVertex(p):
@@ -99,27 +99,66 @@ def pickvertex():
     return random.choice( range(len(vertices) ))
 
 def lineFromPoints(p1,p2):
-    #TODO
-    return None
-
+    """Compute slope of line from two points."""
+    dx = (p1[0] - p2[0])
+    w = (p1[1] - p2[1]) / dx
+    b = p1[1] - w*p1[0]
+    return w, b
 
 def pointPointDistance(p1,p2):
-    #TODO
-    return 0
+    """ Compute Euclidean distance between two points in 2D. """
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 def closestPointToPoint(G,p2):
-    #TODO
-    #return vertex index
-    return 0
+    """ Return index of closest vertex on the graph to generated point """
+    distances = []
+    for i, v in enumerate(G[nodes]):
+        distances.append((i, pointPointDistance(vertices[v], p2)))
+
+    # sorting according to distance
+    distances.sort(key=lambda x: x[1])
+    
+    # return index of the closest vertex on the graph
+    return distances[0][0]
 
 def lineHitsRect(p1,p2,r):
-    #TODO
+    # segment slope
+    w, b = lineFromPoints(p1,p2) # y = wx + b for x [p1[0], p2[0]] and y [p1[1], p2[1]]
+
+    # see if line intersects with all 4 sides of rectangle (first check if line to line intersection, then check if intersection point within segment)
+    x1_y = r[0], w*r[0] + b # left edge using x1
+    x2_y = r[2], w*r[2] + b # right edge using x2
+    y1_x = (r[1] - b) / w, r[1] # bottom edge using y1
+    y2_x = (r[3] - b) / w, r[3] # top edge using y2
+
+    # get the four corners of the rectangle
+    A = r[0], r[1] # upper left
+    B = r[2], r[1] # upper right
+    C = r[2], r[3] # right bottom
+    D = r[0], r[3] # left bottom
+
+    for p in [x1_y,x2_y,y1_x,y2_x]:
+        if point_within_range(p, p1, p2) and (point_within_range(p, A, B) or point_within_range(p, B, C) or point_within_range(p, C, D) or point_within_range(p, D, A)):
+            return True
     return False
+
+def point_within_range(point, p1, p2):
+    x_range = [p2[0], p1[0]] if p1[0] > p2[0] else [p1[0], p2[0]]
+    y_range = [p2[1], p1[1]] if p1[1] > p2[1] else [p1[1], p2[1]]
+    return x_range[0] <= point[0] <= x_range[1] and y_range[0] <= point[1] <= y_range[1]
 
 def inRect(p,rect,dilation):
     """ Return 1 in p is inside rect, dilated by dilation (for edge cases). """
-    #TODO
-    return False
+    return rect[0] <= p[0] <= rect[2] and rect[1] <= p[1] <= rect[-1]
+
+def steer(closest_point, x_rand):
+    dx = x_rand[0] - closest_point[0]
+    dy = x_rand[1] - closest_point[1]
+    theta = math.atan2(dy, dx)
+
+    y = math.sin(theta) * args.step_size
+    x = math.cos(theta) * args.step_size
+    return [closest_point[0] + x, closest_point[1] + y]
 
 def rrt_search(G, tx, ty, canvas):
     #TODO
@@ -130,7 +169,10 @@ def rrt_search(G, tx, ty, canvas):
     n=0
     nsteps=0
     while 1:
+        # 1. sample random point in free space
         p = genPoint()
+
+        # 2. find nearest point on the graph to sampled point
         v = closestPointToPoint(G,p)
 
         if visualize:
@@ -140,18 +182,28 @@ def rrt_search(G, tx, ty, canvas):
                 canvas.events()
                 n=0
 
+        # compute x_new in the direction of tthe generated point
+        p = steer(vertices[v], x_rand=p)
 
+        hit_obstacle = False
         for o in obstacles:
-            #if inRect(p,o,1):
+            # print(o)
+            # canvas.showRect(o, outline='red', fill='red')
+            # canvas.events()
             if lineHitsRect(vertices[v],p,o) or inRect(p,o,1):
-                print("TODO")
-                #... reject
+                hit_obstacle = True
+                break
+            # canvas.showRect(o, outline='red', fill='blue')
 
-        k = pointToVertex( p )   # is the new vertex ID
+        # if the line between the sampled point and the closest point on the graph hits an obstacle, sample new point.
+        if hit_obstacle: continue
+
+        # 3. take a step in the direction of the sampled point if obstacle free
+        k = pointToVertex(p) # is the new vertex ID
         G[nodes].append(k)
-        G[edges].append( (v,k) )
+        G[edges].append((v,k))
         if visualize:
-            canvas.polyline(  [vertices[v], vertices[k] ]  )
+            canvas.polyline([vertices[v], vertices[k]])
 
         if pointPointDistance( p, [tx,ty] ) < SMALLSTEP:
             print("Target achieved.", nsteps, "nodes in entire tree")
