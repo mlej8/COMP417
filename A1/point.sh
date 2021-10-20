adb3ad604f5c54609118a6cba8b01298175ba8c4
Er Jun Li <er.li@mail.mcgill.ca>
2021-10-20 00:15:50 -0400

    Point robot experiment script

--- A1/point.sh
+++ A1/point.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-python rrt_planner_point_robot.py --start_pos_x 10 --start_pos_y 270 --target_pos_x 900 --target_pos_y 30 --rrt_sampling_policy gaussian
+for step_size in 2 4 6 8 10 12 14 16 18 20
+do
+    echo "Step_size $step_size"
+for i in `seq 1 10`
+do
+    echo "Iteration $i"
+    python rrt_planner_point_robot.py \
+        --start_pos_x 10 \
+        --start_pos_y 270 \
+        --target_pos_x 900 \
+        --target_pos_y 30 \
+        --rrt_sampling_policy gaussian \
+        --seed $i \
+        --step_size $step_size >> point.log
+done
+done
