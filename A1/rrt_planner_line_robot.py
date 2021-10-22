12cd98a3a42e5686b99792493d3df64ff87c46c5
Er Jun Li <er.li@mail.mcgill.ca>
2021-10-22 18:38:24 -0400

    Remove theta sampling

--- A1/rrt_planner_line_robot.py
+++ A1/rrt_planner_line_robot.py
@@ -79,8 +79,7 @@ def genPoint():
         if y<0: bad = 1
         if x>XMAX: bad = 1
         if y>YMAX: bad = 1
-    theta = random.random() * 2 * math.pi # uniformly sample [0, 2pi]
-    return [x,y], theta
+    return [x,y]
 
 def returnParent(k, canvas, G):
     """ Return parent note for input node k. """
@@ -103,6 +102,8 @@ def pickvertex():
 def lineFromPoints(p1,p2):
     """Compute slope of line from two points."""
     dx = p1[0] - p2[0]
+    if dx == 0:
+        raise Exception("Vertical line.")
     w = (p1[1] - p2[1]) / dx
     b = p1[1] - w * p1[0]
     return w, b
@@ -125,10 +126,33 @@ def closestPointToPoint(G,p2):
     return index
 
 def lineHitsRect(p1,p2,r):
+    dx = p1[0] - p2[0] 
+    if dx == 0:
+        if r[0] <= p1[0] <= r[2]:
+            largest_point = (p2[1],p1[1]) if p1[1] > p2[1] else (p1[1], p2[1])
+            if r[1] > largest_point[1] or largest_point[0] > r[3]:
+                return False
+            else:
+                return True
+        else:
+            return False
+
     # segment slope
     w, b = lineFromPoints(
         p1, p2
     )  # y = wx + b for x [p1[0], p2[0]] and y [p1[1], p2[1]]
+
+    # if we have a horizontal line
+    if w == 0:
+        if r[1] <= b <= r[3]:
+            largest_point = (p2[0],p1[0]) if p1[0] > p2[0] else (p1[0], p2[0])
+            if r[0] > largest_point[1] or largest_point[0] > r[2]:
+                return False
+            else:
+                return True
+        else:
+            return False
+        
 
     # see if line intersects with all 4 sides of rectangle (first check if line to line intersection, then check if intersection point within segment)
     x1_y = r[0], w * r[0] + b  # left edge using x1
@@ -179,6 +203,64 @@ def steer(closest_point, x_rand):
     else: 
         return x_rand
 
+def can_turn(current_pos, current_orientation, target_orientation, obstacles, canvas):
+    """ See if the robot can rotate w.r.t the center of the line robot to the target orientation. """
+
+    # rad orientation to degrees and get the positive angle
+    target_orientation_deg = 360 + round(math.degrees(target_orientation)) if round(math.degrees(target_orientation)) < 0 else round(math.degrees(target_orientation))
+    current_orientation_deg = 360 + round(math.degrees(current_orientation)) if round(math.degrees(current_orientation)) < 0 else round(math.degrees(current_orientation))
+
+    turn_left, turn_right = True, True
+    
+    padded = False
+    if target_orientation_deg < current_orientation_deg: 
+        padded = True
+        target_orientation_deg += 360
+    
+    # if current_orientation_deg > target_orientation_deg:
+    for angle in range(current_orientation_deg, target_orientation_deg + 1):
+        angle = angle % 360
+        robot_head, robot_tail = get_robot_head_tail(current_pos, math.radians(angle))
+        if not point_within_canvas(robot_head) or not point_within_canvas(robot_tail): break
+        for o in obstacles:
+            if lineHitsRect(robot_head, robot_tail, o):
+                canvas.canvas.create_line([robot_head, robot_tail],fill='yellow', width=3)
+                canvas.events()
+                turn_left = False
+                break
+        if not turn_left:
+            break
+        else:
+            return True
+
+    for angle in range(current_orientation_deg if padded else target_orientation_deg, target_orientation_deg - 361 if padded else current_orientation_deg - 1, -1):
+        robot_head, robot_tail = get_robot_head_tail(current_pos, math.radians(angle))
+        if not point_within_canvas(robot_head) or not point_within_canvas(robot_tail): break
+        for o in obstacles:
+            if lineHitsRect(robot_head, robot_tail, o):
+                canvas.canvas.create_line([robot_head, robot_tail],fill='yellow', width=3)
+                canvas.events()
+                turn_right = False
+                break
+        if not turn_right:
+            break
+        else:
+            return True
+    return False
+
+def get_robot_head_tail(position, orientation):
+    dx = math.cos(orientation) * (args.robot_length/2)
+    dy = math.sin(orientation) * (args.robot_length/2)
+    robot_head = position[0] + dx, position[1] + dy
+    robot_tail = position[0] - dx, position[1] - dy
+    return robot_head, robot_tail
+
+def get_turn_angle(p1, p2):
+    dx = p1[0] - p2[0]
+    dy = p1[1] - p2[1]
+    theta_rad = math.atan2(dy, dx)
+    return theta_rad
+
 def rrt_search(G, tx, ty, canvas):
     #TODO
     #Fill this function as needed to work ...
@@ -187,9 +269,10 @@ def rrt_search(G, tx, ty, canvas):
     global sigmax_for_randgen, sigmay_for_randgen
     n=0
     nsteps=0
+    current_orientation = args.start_pos_theta
     while 1:
         nsteps += 1
-        p, theta = genPoint()
+        p = genPoint()
         v = closestPointToPoint(G,p)
 
         if visualize:
@@ -199,35 +282,37 @@ def rrt_search(G, tx, ty, canvas):
                 canvas.events()
                 n=0
 
+        nearest_vertex = vertices[v]
+
         # compute x_new in the direction of tthe generated point
-        p = steer(vertices[v], x_rand=p)
+        p = steer(nearest_vertex, x_rand=p)
         
         # NOTE: we assume that (x,y) represent the middle of the line robot and that theta is expressed w.r.t the x-axis
-        # NOTE: we assume that if x_new with orientation theta is a valid position, then the local planner of the robot is able to get there
-        dx = math.cos(theta) * (args.robot_length/2)
-        dy = math.sin(theta) * (args.robot_length/2)
-        robot_head = p[0] + dx, p[1] + dy
-        robot_tail = p[0] - dx, p[1] - dy
-
+        # NOTE: we assume that x_new's orientation will be defined by the angle between the nearest neighbor and itself.
+        theta = get_turn_angle(p,nearest_vertex)
+        robot_head, robot_tail = get_robot_head_tail(p, theta)
+        
         hit_obstacle = True
-        # if validTargetPos():
+        
         for o in obstacles:
-            if lineHitsRect(vertices[v],p,o) or inRect(p,o,1) or inRect(robot_tail,o,1) or inRect(robot_head,o,1) or lineHitsRect(robot_head, robot_tail, o) or not point_within_canvas(robot_head) or not point_within_canvas(robot_tail):
+            if inRect(p,o,1) or inRect(robot_tail,o,1) or inRect(robot_head,o,1) \
+                or lineHitsRect(nearest_vertex,p,o) or lineHitsRect(robot_head, robot_tail, o) \
+                or not point_within_canvas(robot_head) or not point_within_canvas(robot_tail):
                 break
         else:
             hit_obstacle = False
-        
-        # if the line between the sampled point and the closest point on the graph hits an obstacle, sample new point.
-        if hit_obstacle:
+
+        # can't turn towards target point or trajectory or resulting position hit obstacle
+        if hit_obstacle or not can_turn(current_pos=nearest_vertex, current_orientation=current_orientation, obstacles=obstacles, target_orientation=theta, canvas=canvas):
             continue
-        
+        current_orientation = theta
         # visualize line robot
         canvas.canvas.create_line([robot_head, robot_tail],fill='green', width=1)
         k = pointToVertex( p )   # is the new vertex ID
         G[nodes].append(k)
         G[edges].append( (v,k) )
         if visualize:
-            canvas.polyline(  [vertices[v], vertices[k] ]  )
+            canvas.polyline(  [nearest_vertex, vertices[k] ]  )
 
         if pointPointDistance( p, [tx,ty] ) < SMALLSTEP:
             print("Target achieved.", len(vertices), "nodes in entire tree")
